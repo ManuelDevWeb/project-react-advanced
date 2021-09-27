99bc609cf1053105fd065f39e314074da46fbed0
unknown <manueldeveloper17@gmail.com>
2021-09-27 13:19:45 -0500

    Add PWA config

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,8 @@
-// Importando plugin HTML
+// Importando plugins
 const HTMLWebpackPlugin = require('html-webpack-plugin');
+const WebpackPwaManifestPlugin = require('webpack-pwa-manifest');
+
+const path = require('path');
 
 module.exports = {
     // Ouput de la compilación
@@ -12,6 +15,18 @@ module.exports = {
         // Instanciando plugin de HTML
         new HTMLWebpackPlugin({
             template: 'src/index.html'
+        }),
+        // Instanciando plugin PWA
+        new WebpackPwaManifestPlugin({
+            name: 'Petgram - Tu App de fotos de mascotas',
+            short_name: 'Pettgram 🐶',
+            description: 'Con Petgram puedes encontrar y subir fotos de animales domésticos.',
+            background_color: '#FFF',
+            theme_color: '#b1a',
+            icons: [{
+                src: path.resolve('src/assets/icon.png'),
+                sizes: [96, 128, 192, 256, 384, 512]
+            }]
         })
     ],
     module: {
